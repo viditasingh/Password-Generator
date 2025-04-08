1fd3c7018a80f36e8c69d377434d6c0bae76e7b4
Vidita Singh <vidita1812@gmail.com>
2025-04-09 02:08:14 +0530

    add basic UI

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,54 @@ function App() {
 
     return(
     <>
-      
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-blue-300 bg-gray-700 py-0.5 pb-1">
+        <h1 className='text-white text-center my-3'>Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+            <input 
+                type="text"
+                value={password} 
+                className='outline-none w-full py-1 px-3 bg-amber-50'
+                placeholder='Password'
+                readOnly
+            />
+            <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+            <div className="flex items-center gap-x-1">
+                <input 
+                    type="range"
+                    min={6}
+                    max={100}
+                    value={length}
+                    className='cursor-pointer'
+                    onChange={(e)=>{setLength(e.target.value)}}
+                />
+                <label>Length: {length}</label>
+            </div>
+            <div className="flex items-center gap-x-1">
+                <input 
+                    type="checkbox"
+                    defaultChecked = {numAllowed}
+                    id='numInput'
+                    onChange={()=>{
+                        setNumAllowed ((prev)=> !prev);
+                    }}
+                />
+                <label htmlFor="numInput">Numbers</label>
+            </div>
+            <div className="flex items-center gap-x-1">
+                <input 
+                    type="checkbox"
+                    defaultChecked = {charAllowed}
+                    id='charInput'
+                    onChange={()=>{
+                        setCharAllowed ((prev)=> !prev);
+                    }}
+                />
+                <label htmlFor="charInput">Characters</label>
+            </div>
+        </div>
+      </div>
     </>)
 }
 
